a66f9c29d7eab319ede479301776505a201a40ff
Stephen Huang <huangstephen9@gmail.com>
2020-09-21 16:50:51 -0400

    Cheese model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user')
+const Cart = require('./cart')
+const Cheese = require('./cheese')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +15,11 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+ User.hasOne(Cart);
+
 module.exports = {
-  User
+  User,
+  Cart,
+  Cheese
 }
