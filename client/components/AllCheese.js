e48855ee5c999b7d9794b19a67f50065f7087855
Vito S <vrs815@gmail.com>
2020-09-23 12:44:54 -0400

    feat:

--- client/components/AllCheese.js
+++ client/components/AllCheese.js
@@ -1,21 +1,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {fetchCheeses} from '../store/cheeses'
 
 class AllCheese extends React.Component {
-  constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
+  // constructor() {
+  //   super()
+  // }
+  componentDidMount() {
+    this.props.getCheeses()
+    console.log(this.props.cheeses)
   }
-  componentDidMount() {}
 
   render() {
-    return <div>hello</div>
+    return (
+      <div>
+        {this.props.cheeses ? (
+          <div>
+            {this.props.cheeses.map(cheese => {
+              return (
+                <div key={cheese.id}>
+                  <img width={100} height={100} src={cheese.imageUrl} />
+
+                  <Link to={`cheeses/${cheese.id}`}>
+                    <div>{cheese.name}</div>
+                  </Link>
+                  <button type="button">Add to Cart</button>
+                </div>
+              )
+            })}
+          </div>
+        ) : (
+          <p>No cheese</p>
+        )}
+      </div>
+    )
   }
 }
 
-const mapState = state => {}
+const mapState = state => {
+  return {cheeses: state.cheesesReducer.cheeses}
+}
 
-const mapDispatch = dispatch => {}
+const mapDispatch = dispatch => {
+  return {
+    getCheeses: () => dispatch(fetchCheeses())
+  }
+}
 
 export default connect(mapState, mapDispatch)(AllCheese)
